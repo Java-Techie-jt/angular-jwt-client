f6afbae3435dc7be737160cf36ce7b6393d6b588
Basant kumar Hota <basahota@users.noreply.github.com>
2020-02-19 19:42:55 +0530

    Update security.component.ts

--- src/app/security/security.component.ts
+++ src/app/security/security.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { JwtClientService } from '../jwt-client.service';
-import { AuthRequest } from './authRequest';
 
 @Component({
   selector: 'app-security',
@@ -9,23 +8,27 @@ import { AuthRequest } from './authRequest';
 })
 export class SecurityComponent implements OnInit {
 
-  authRequest: AuthRequest = {
-    "userName": "javatechie",
-    "password": "password"
+  authRequest:any={
+    "userName":"javatechie",
+    "password":"password"
   };
 
-  token: string;
-  response: string;
+  response:any;
 
-  constructor(private service: JwtClientService) { }
+  constructor(private service:JwtClientService) { }
 
   ngOnInit() {
-    let resp = this.service.generateToken(this.authRequest);
-    resp.subscribe(data =>{
-      this.service.welcome(data).subscribe(data => this.response = data)
-    } );
-    //console.log("Token : "+JSON.stringify(data));
-   // this.service.welcome().subscribe(data => this.response = data);
+    this.getAccessToken(this.authRequest);
   }
 
+  public getAccessToken(authRequest){
+let resp=this.service.generateToken(authRequest);
+resp.subscribe(data=>this.accessApi(data));
+  }
+
+
+  public accessApi(token){
+let resp=this.service.welcome(token);
+resp.subscribe(data=>this.response=data);
+  }
 }
