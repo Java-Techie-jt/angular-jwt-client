e20dd4ce25ce3f7cd28a368bb99ca18270ceb68a
Basant kumar Hota <basahota@users.noreply.github.com>
2020-02-19 19:45:15 +0530

    Update jwt-client.service.ts

--- src/app/jwt-client.service.ts
+++ src/app/jwt-client.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AuthRequest } from './security/authRequest';
 
 
 @Injectable({
